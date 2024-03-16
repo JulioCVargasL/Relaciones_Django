a2a3d76703e2baa51efaa8a994b00b878200eace
JulioVargas <2.0.jcvl92@gmail.com>
2024-03-16 17:48:09 -0500

    Segundo Commit

--- many_to_one/views.py
+++ many_to_one/views.py
@@ -1,17 +1,36 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Reporter, Article
+from datetime import date
 
 # Create your views here.
 
 def inicio(request):
-  repo1 = Reporter(first_name="Julio", last_name="Vargas", email="Julio_exmple.com")
+  # repo1 = Reporter(first_name="Julio", last_name="Vargas", email="Julio_exmple.com")
+  repo1 = Reporter.objects.get(id=1)
+  # repo1.save()
 
-  repo1.save()
+  # repo2 = Reporter(first_name="Tulio", last_name="Triviño", email="Tulio_exmple.com")
+  repo2 = Reporter.objects.get(id=2)
+  # repo2.save()
 
-  repo2 = Reporter(first_name="Tulio", last_name="Triviño", email="Tulio_exmple.com")
+  # Article.objects.create(
+  #   headline="Primer articulo", 
+  #   pub_date= date(2023, 5, 12), 
+  #   reporter=repo1
+  #   )
+  # Article.objects.create(
+  #   headline="Segundo Articulo", 
+  #   pub_date= date(2024, 5, 12), 
+  #   reporter=repo1
+  #   )
+  # Article.objects.create(
+  #   headline="Articulo del Tulio", 
+  #   pub_date= date(2024, 2, 29), 
+  #   reporter=repo2
+  #   )
+  # art1.reporter
 
-  repo2.save()
 
 
   return HttpResponse('hola esto es de muchos a uno')
